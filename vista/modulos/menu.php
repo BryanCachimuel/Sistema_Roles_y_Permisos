09f2078a83fdc5d3ad771353fbe784b7248ee09c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-29 19:46:55 -0500

    agregando los dos módulos con los cuales va a contar el sistema y con ello se pueda redirigir cada módulo a su respectiva página administrativa

--- vista/modulos/menu.php
+++ vista/modulos/menu.php
@@ -7,39 +7,27 @@
               <div class="pull-left image">
                   <img src="vista/imagenes/usuarios/foto.jpg" class="img-circle" alt="User Image">
               </div>
-              <!--<div class="pull-left info">
+              <div class="pull-left info">
                   <p>Alexander Pierce</p>
                   <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-              </div>-->
-          </div>
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-              <div class="input-group">
-                  <input type="text" name="q" class="form-control" placeholder="Search...">
-                  <span class="input-group-btn">
-                      <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                      </button>
-                  </span>
               </div>
-          </form>
-          <!-- /.search form -->
+          </div>
+
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu" data-widget="tree">
               <li class="header">Menú de Navegación</li>
 
               <li>
-                <a href="grupos">
+                <a href="index.php?pagina=usuarios">
                     <i class="fa fa-user"> <span>Usuarios</span> </i>
                 </a>
               </li>
 
-              
               <li>
-                <a href="grupos">
+                <a href="index.php?pagina=roles">
                     <i class="fa fa-link"> <span>Roles</span> </i>
                 </a>
-              </li>
-        
+              </li>  
           </ul>
       </section>
       <!-- /.sidebar -->
