f1e8a986ef5ec56e15cdff29d3f49c81bd42225f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-29 19:47:54 -0500

    modificando la vista principal en donde se limpia los componentes no necesarios para el proyecto

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -19,13 +19,6 @@
        folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="vista/recursos/dist/css/skins/_all-skins.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
@@ -40,51 +33,18 @@
 
       <?php include "modulos/menu.php"; ?>
 
-        <!-- =============================================== -->
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    Blank page
-                    <small>it all starts here</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                    <li><a href="#">Examples</a></li>
-                    <li class="active">Blank page</li>
-                </ol>
-            </section>
 
-            <!-- Main content -->
-            <section class="content">
+            <?php
+                if(isset($_GET["pagina"])){
+                    if($_GET["pagina"] == "usuarios" || $_GET["pagina"] == "roles")  {
+                        include "paginas/".$_GET["pagina"].".php";
+                    }
+                }
+            ?>
 
-                <!-- Default box -->
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Title</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                                <i class="fa fa-minus"></i></button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                                <i class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        Start creating your amazing application!
-                    </div>
-                    <!-- /.box-body -->
-                    <div class="box-footer">
-                        Footer
-                    </div>
-                    <!-- /.box-footer-->
-                </div>
-                <!-- /.box -->
-
-            </section>
-            <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
 
