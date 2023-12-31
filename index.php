b648e153aae8eb23e5c35a8f5456dd1164a2c6ed
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-30 22:22:02 -0500

    agregando el controlador y el modelo de usuarios en el archivo index.php del proyecto

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
+/*
+    El archivo .htaccess -> sirve para ordenar de mejor manera las urls y tengan una mejor presentación,
+                            
+    RewriteRule ^([-a-zA-Z0-9/]+)$ index.php?pagina=$1 -> esta parte hace que ya no aparezca en la utl esta sección index.php?pagina 
+                                                          y solo aparezca las rutas de los nombres de las paginas a visitar
+*/
+
 /* llamando al controlador */
 include "controlador/plantilla.controlador.php";
+include "controlador/usuario.controlador.php";
+
+include "modelo/usuario.modelo.php";
 
 /* utilizamos POO para poder llamar a la clase creada en el archivo plantilla.controlador.php*/
 $plantilla = new ControladorPlantilla();
