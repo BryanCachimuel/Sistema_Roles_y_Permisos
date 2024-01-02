39a19d0401b7e64f6ae70fb3404575e117a9f7c1
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-01 21:36:11 -0500

    creando la función mdlguardarUsuarios donde se realiza el procedimiento para guardar los datos que se obtengan mediante el controlador que asu vez este obtienes los datos del formulario de registros de usuarios

--- modelo/usuario.modelo.php
+++ modelo/usuario.modelo.php
@@ -14,6 +14,26 @@ class mdlUsuarios {
         $stmt->close();
         $stmt = null;
     }
+
+    static public function mdlguardarUsuarios($tabla, $datos){
+        $stmt = Conexion::conectar()->prepare("INSERTO INTO $tabla(usuario,password,nombre,foto,rol) VALUES (:USUARIO, :PASS, :NOMBRE, :FOTO, :ROL)");
+        $stmt->bindParam(":NOMBRE",$datos["nom_usuario"], PDO::PARAM_STR);
+        $stmt->bindParam(":USUARIO",$datos["nom_user"], PDO::PARAM_STR);
+        $stmt->bindParam(":PASS",$datos["pass_user"], PDO::PARAM_STR);
+        $stmt->bindParam(":ROL",$datos["rol_user"], PDO::PARAM_INT);
+        $stmt->bindParam(":FOTO",$datos["foto"], PDO::PARAM_STR);
+
+        if($stmt->execute()){
+            return "ok";
+        }else{
+            echo "error";
+        }
+
+        $stmt->close();
+        $stmt = null;
+    }
+
+    
 }
 
 ?>
