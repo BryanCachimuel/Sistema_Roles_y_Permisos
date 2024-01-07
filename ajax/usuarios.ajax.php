c39f341cdcf212c25eba7922e2e0294025df6047
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-06 21:43:04 -0500

    creando la función ajaxEditarUusuarios y realizando un proceso para validar que si existe un id del usuario a editar

--- ajax/usuarios.ajax.php
+++ ajax/usuarios.ajax.php
@@ -1,9 +1,27 @@
 <?php
 
+require_once "../controlador/usuario.controlador.php";
+require_once "../modelo/usuario.modelo.php";
+
 class AjaxUsuarios {
 
-    
+    public $idUsuario;
 
+    public function ajaxEditarUsuarios(){
+        $item = "usuarios";
+        $valor = $this->idUsuario;
+
+        $respuesta = ctrlUsuarios::ctrlMostrarUsuarios();
+        return $respuesta;
+    }
+
+}
+
+/* proceso para editar usuarios */
+if(isset($_POST["idUsuario"])){
+    $editar = new AjaxUsuarios();
+    $editar->idUsuario = $_POST["idUsuario"];
+    $editar->ajaxEditarUsuarios();
 }
 
 ?>
