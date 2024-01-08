a7a1da443868f837d3c9325173b021f2e8afe048
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-07 20:28:50 -0500

    creando la función ctrMostrarRoles2

--- controlador/roles.controlador.php
+++ controlador/roles.controlador.php
@@ -7,6 +7,12 @@ class ctrlRoles{
         $respuesta = mdlRoles::mdlMostrarRoles($tabla, $item, $valor);
         return $respuesta;
     }
+
+    static public function ctrMostrarRoles2($item, $valor){
+        $tabla = "roles";
+        $respuesta = mdlRoles::mdlMostrarRoles($tabla, $item, $valor);
+        return $respuesta;
+    }
     
 
 }
