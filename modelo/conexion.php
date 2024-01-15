98f49f263e5edab35b538963c58eb912dbe33cf1
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-14 21:41:05 -0500

    realizando la conexión adecuada hacia la base de datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,16 +1,14 @@
-<?php
+<?php 
 
-class Conexion {
+class Conexion{
 
-    static public function conectar() {
+    static public function conectar(){
+
         $link = new PDO("mysql:host=localhost;dbname=roles_mvc","root","");
-
-        /* esta nomenclatura permite ingresar a la base de datos diferentes tipos de caracteres como emogis */
         $link->exec("set names utf8mb4");
 
         return $link;
     }
-
 }
 
 ?>
