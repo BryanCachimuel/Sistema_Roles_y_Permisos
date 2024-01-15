a5f0f0acc8f6a53ec28e48ac30735a2d7729b9de
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-14 22:00:24 -0500

    para que el proyecto funcione en este archivo se importa los controladores y modelos de Usuario y Roles

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<?php
+<?php 
 
-/*
-    El archivo .htaccess -> sirve para ordenar de mejor manera las urls y tengan una mejor presentación,
-                            
-    RewriteRule ^([-a-zA-Z0-9/]+)$ index.php?pagina=$1 -> esta parte hace que ya no aparezca en la utl esta sección index.php?pagina 
-                                                          y solo aparezca las rutas de los nombres de las paginas a visitar
-*/
+    include "controlador/plantilla.controlador.php";
+    include "controlador/usuarios.controlador.php";
+    include "controlador/roles.controlador.php";
 
-/* llamando al controlador */
-include "controlador/plantilla.controlador.php";
-include "controlador/usuario.controlador.php";
-include "controlador/roles.controlador.php";
+    include "modelo/usuarios.modelo.php";
+    include "modelo/roles.modelo.php";
 
-include "modelo/usuario.modelo.php";
-include "modelo/roles.modelo.php";
 
-/* utilizamos POO para poder llamar a la clase creada en el archivo plantilla.controlador.php*/
-$plantilla = new ControladorPlantilla();
-
-$plantilla->ctrlPlantilla();
+    $plantilla = new ControladorPlantilla();
+    $plantilla->ctrPlantilla();
 
 ?>
