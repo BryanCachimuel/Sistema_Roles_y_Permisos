d937d4673df0a20632312d3406c9e2294201219a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-24 22:13:32 -0500

    realizando el proceso de encriptación y envio de datos de confirmación de los usuario hacia los correos de los mismos

--- models/Email.php
+++ models/Email.php
@@ -18,11 +18,26 @@ require_once("../models/Usuario.php");
 
 class Email extends PHPMailer{
     /*TODO: creamos dos variables con los datos del correo administrable y su contraseña */
-    protected $gCorreo = 'bryanloyo56@gmail.com';
-    protected $gContrasenia = 'blsep25CLnode*';
+    protected $gCorreo = 'info@rixlercode.net';
+    protected $gContrasenia = 'nodeXdtesf*';
+
+    /*TODO: proceso de encriptación del id del usuario registrado */
+    private $key="MesaDePartesRixlerC";
+    private $cipher="aes-256-cbc";
 
     /*TODO: para enviar un correo se debe enviar como parámetro el id para saber de que usuario es */
     public function registrar($usu_id){
+
+        /*TODO: Instancia de la clase Conectar con la cual se obtendra la función ruta  */
+        $conexion = new Conectar();
+
+        $usuario = new Usuario();
+        $datos = $usuario->get_usuario_id($usu_id);
+
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->cipher));
+        $cifrado = openssl_encrypt($usu_id, $this->cipher, $this->key, OPENSSL_RAW_DATA, $iv);
+        $textoCifrado = base64_encode($iv . $cifrado);
+
         $this->isSMTP();
         $this->Host = 'smtp-relay.brevo.com'; // este es el host de hostinger
         $this->Port = 587;  // puerto que usa hostinger
@@ -34,12 +49,15 @@ class Email extends PHPMailer{
         $this->setFrom($this->gCorreo,"Registro en Mesa de Partes del Sistema");// se setea el mensaje que aparece en el frontend
         
         $this->CharSet = 'UTF8';// se setea los caracteres en utf8
-        $this->addAddress("bryanloyo56@gmail.com"); // se envia a este correo una copia de lo que se mande del correo corporativo
+        $this->addAddress($datos[0]["usu_correo"]); // se envia a este correo una copia de lo que se mande del correo corporativo
         $this->isHTML(true); // se envia lo que es un html 
         $this->Subject = "Mesa de Partes";
 
+        /*TODO: url de la ruta donde se encuentra la vista de confirmación de correo */
+        $url = $conexion->ruta() . "view/confirmar/?id=".$textoCifrado;
+
         $cuerpo = file_get_contents("../assets/email/registrar.html");// variable en donde se va a enviar el cuerpo del correo 
-        $cuerpo = str_replace("xusuariocorreo",$usu_id,$cuerpo); //xusuariocorreo es un código para reemplazar  ciertas partes del código del archivo de confirmación de correo
+        $cuerpo = str_replace("xlinkcorreourl",$url,$cuerpo); //xusuariocorreo es un código para reemplazar  ciertas partes del código del archivo de confirmación de correo
         
         $this->Body = $cuerpo;
         $this->AltBody = strip_tags("Confirmar Registro");// se agrega una etiqueta al cuerpo
