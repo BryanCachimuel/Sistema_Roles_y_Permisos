22160e81275b77f77bc52ddedcb2d2efb771c413
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-29 21:39:19 -0500

    dando funcionalidad al boton btnrecuperar mediante jQuery en donde se le agrega validaciones para el campo correo y se verifica que se ingrese información y realice el procedimiento de recuperación de contraseña

--- view/recuperar/recuperar.js
+++ view/recuperar/recuperar.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
 
-    $.post("../../controller/email.php?op=recuperar", {usu_correo : "blcl@gmail.com"}, function(data){
-    });
+    
+});
 
+$(document).on("click","#btnrecuperar", function(){
+
+    var usu_correo = $('#usu_correo').val();
+
+    if(usu_correo === ""){
+        Swal.fire({
+            title: "Recuperar",
+            text: "El campo de correo electrónico está vacío, por favor validar",
+            icon: "error",
+            confirmButtonColor: "#5156be",
+        });
+        
+    }else{
+
+        $.post("../../controller/email.php?op=recuperar", {usu_correo : usu_correo}, function(datos){
+            if(datos == 1){
+                Swal.fire({
+                    title: "Recuperar",
+                    text: "Se cambio la contraseña y se envió a su correo electrónico",
+                    icon: "success",
+                    confirmButtonColor: "#5156be",
+                });
+            }else{
+                Swal.fire({
+                    title: "Recuperar",
+                    text: "El correo electrónico no existe",
+                    icon: "error",
+                    confirmButtonColor: "#5156be",
+                });
+            }
+        });
+    }
 });
