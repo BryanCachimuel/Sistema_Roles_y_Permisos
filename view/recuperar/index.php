d6f7be2c6c481b5851fb9199b06ec6ff6aee06a8
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-30 21:28:57 -0500

    agregando mediante un include_once la sección de carrucel

--- view/recuperar/index.php
+++ view/recuperar/index.php
@@ -85,67 +85,7 @@
                             <li></li>
                         </ul>
                         <!-- end bubble effect -->
-                        <!-- end bubble effect -->
-                        <div class="row justify-content-center align-items-center">
-                            <div class="col-xl-7">
-                                <div class="p-0 p-sm-4 px-xl-0">
-                                    <div id="reviewcarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                                        <div class="carousel-indicators carousel-indicators-rounded justify-content-start ms-0 mb-0">
-                                            <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                            <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                        </div>
-                                        <!-- end carouselIndicators -->
-                                        <div class="carousel-inner">
-                                            <div class="carousel-item active">
-                                                <div class="testi-contain text-white">
-                                                    <i class="bx bxs-quote-alt-left text-success display-6"></i>
-
-                                                    <h4 class="mt-4 fw-medium lh-base text-white">“Mesa de partes
-                                                        construida para
-                                                        dar un mejor servicio a los usuarios que se registren dentro
-                                                        de la plataforma.”
-                                                    </h4>
-                                                    <div class="mt-4 pt-3 pb-5">
-                                                        <div class="d-flex align-items-start">
-                                                            <div class="flex-shrink-0">
-                                                                <img src="../../assets/picture/avatar-1-1.jpg" class="avatar-md img-fluid rounded-circle" alt="...">
-                                                            </div>
-                                                            <div class="flex-grow-1 ms-3 mb-4">
-                                                                <h5 class="font-size-18 text-white">Richard Drews</h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="carousel-item">
-                                                <div class="testi-contain text-white">
-                                                    <i class="bx bxs-quote-alt-left text-success display-6"></i>
-
-                                                    <h4 class="mt-4 fw-medium lh-base text-white">“Mesa de partes
-                                                        contiene contenido necesario para los usuarios y les
-                                                        servira para muchas cosas
-                                                        dentro de su área de vida social.”</h4>
-                                                    <div class="mt-4 pt-3 pb-5">
-                                                        <div class="d-flex align-items-start">
-                                                            <div class="flex-shrink-0">
-                                                                <img src="../../assets/picture/avatar-2-1.jpg" class="avatar-md img-fluid rounded-circle" alt="...">
-                                                            </div>
-                                                            <div class="flex-grow-1 ms-3 mb-4">
-                                                                <h5 class="font-size-18 text-white">Roxana Díaz
-                                                                </h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!-- end carousel-inner -->
-                                    </div>
-                                    <!-- end review carousel -->
-                                </div>
-                            </div>
-                        </div>
+                        <?php require_once("../home/index.php"); ?>
                     </div>
                 </div>
                 <!-- end col -->
