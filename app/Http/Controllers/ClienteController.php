26d803a1341a7e8534ba0f07b7add2ae2eb770c5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-02-11 21:49:03 -0500

    realizando el método index que sirve para listar todos los datos de los clientes registrados

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -12,7 +12,8 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        return "Lista de clientes";
+        $cliente = Client::all();
+        return view('sistema.listarCliente',compact('cliente'));
     }
 
     /**
