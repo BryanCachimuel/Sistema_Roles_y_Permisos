73848548c352df7852ce307b900f57b2a1d17614
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-11 17:01:44 -0500

    Creando dos registros para la entidad users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Bryan C',
+            'email' => 'blcl@gmail.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Melanie M',
+            'email' => 'mlmr@gmail.com',
         ]);
     }
 }
