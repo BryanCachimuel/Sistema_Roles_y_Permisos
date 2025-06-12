364dac933aa109127c9e7890e12dc301b98d6b57
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-11 22:53:40 -0500

    Actualizando el tipo de ingreso de información que puede tener el campo contenido

--- database/migrations/2025_06_11_214926_create_messages_table.php
+++ database/migrations/2025_06_11_214926_create_messages_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
