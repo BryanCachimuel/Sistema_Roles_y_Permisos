4bc258efdf3e554ed6b928bd8596fb18544cdd6f
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-12 16:17:44 -0500

    Agregando una tarjeta nueva para que aparezca los mensajes que los usuarios envían desde el formulario de chat

--- resources/views/livewire/manage-messages.blade.php
+++ resources/views/livewire/manage-messages.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form class="bg-white rounded-lg shadow-lg p-6"  wire:submit="save">
+    <form class="bg-white rounded-lg shadow-lg p-6 mb-4"  wire:submit="save">
        
         <x-validation-errors class="mb-4"/>
 
@@ -16,4 +16,35 @@
             </x-button>
         </div>
     </form>
+
+   @if ($mensajes->count())
+         <div class="bg-white rounded-lg shadow-lg p-6">
+        <ul class="space-y-4">
+            @foreach ($mensajes as $message)
+                <li class="flex">
+                    <div class="mr-4 shrink-0">
+                        <img class="h-8 w*8 rounded-full object-cover object-center" src="{{ $message->user->profile_photo_url }}" alt="">
+                    </div>
+
+                    <div class="flex-1">
+                        <p>
+                            <b>
+                                {{ $message->user->name }}
+                            </b>
+
+                            <span class="text-xs text-gray-500">
+                                {{ $message->created_at->diffForHumans() }}
+                            </span>
+                        </p>
+
+                        <div>
+                            {{ $message->content }}
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>    
+   @endif  
+
 </div>
